375105b24ee6eda6343b60f4f3f105207b9e4e90
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-10-05 11:48:15 +0530

    Update Home component to include Footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import MusicPlayer from "./music/MusicPlayer";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./search/Sidebar";
+import Footer from "./Footer";
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "../Auth/firebase";
@@ -23,20 +24,29 @@ export default function Home() {
 
     // Default search
     const DEFAULT_SEARCH = "top hits";
-    const pathName = `/search?searchtxt=${localStorage.getItem("search") || DEFAULT_SEARCH}`;
-    const currentSearch = new URLSearchParams(window.location.search).get("searchtxt");
-    if (!currentSearch) navigate(pathName);
+    const searchText = localStorage.getItem("search")
+    if(!searchText) {
+      localStorage.setItem("search", DEFAULT_SEARCH)
+    }
+    const pathName = `/search?searchTxt=${localStorage.getItem("search")}`;
+    const currentSearch = new URLSearchParams(window.location.search).get("searchTxt");
+    if (!currentSearch) {
+      navigate(pathName);
+    }
 }, []);
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <div className="flex items-start">
         <Sidebar />
         <InputBar />
       </div>
-      <Suspense fallback={<div>...Loading</div>}>
-        <Outlet />
-      </Suspense>
+      <main className="flex-1">
+        <Suspense fallback={<div>...Loading</div>}>
+          <Outlet />
+        </Suspense>
+      </main>
       <MusicPlayer />
-    </>
+      <Footer />
+    </div>
   );
 }
