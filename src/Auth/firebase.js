938d65a57f6db7d31b77a81b6fde7eeb1e6b625f
Vaibhav Sonawane <vaibhavsonawane2345@gmail.com>
2025-10-11 22:03:54 +0530

    Final fix

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAqQi1KT2F1CNacraAG7iGP5_S2UwKdhME",
-  authDomain: "test-music-app-42a1d.firebaseapp.com",
-  projectId: "test-music-app-42a1d",
-  storageBucket: "test-music-app-42a1d.firebasestorage.app",
-  messagingSenderId: "722630245657",
-  appId: "1:722630245657:web:d84ab049f48ade6171d988",
-  measurementId: "G-GEXNZ2M00M",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
